f9e485b4be0cc6a9d5e93a1bca544e2f9c4f1744
lucas98sf <lucas.98sf@gmail.com>
2022-07-13 15:40:58 -0300

    fix: error handling

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ export const handler: APIGatewayProxyHandler = async event => {
 	try {
 		const result = { statusCode: httpStatus.OK, body: event.body! };
 
-		logger.info(JSON.stringify(result));
+		logger.debug(JSON.stringify(result));
 
 		return result;
 	} catch (err) {
 		logger.error(err);
-		return { statusCode: httpStatus.INTERNAL_SERVER_ERROR, body: JSON.stringify(err) };
+		throw err;
 	}
 };
