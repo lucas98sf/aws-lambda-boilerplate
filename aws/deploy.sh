f74f63d1b2edbbacbedb57b242e960414d3877b2
lucas98sf <lucas.98sf@gmail.com>
2022-07-13 14:53:57 -0300

    fix: webpack and deploy

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -3,12 +3,11 @@
 	lambda_name=${PWD##*/}
 	lambda_name=${lambda_name:-/} 
 	aws iam create-role --role-name $lambda_name --assume-role-policy-document file://aws/trust-policy.json
-	aws iam attach-role-policy --role-name $lambda_name --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
-	npm i && npm run build 
-	zip -r -D bundle.zip dist
+	aws iam attach-role-policy --role-name $lambda_name --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole 
+	zip -r -j bundle.zip dist/bundle.js
 	{
 	  aws lambda create-function --function-name $lambda_name \
-	  --zip-file fileb://bundle.zip --handler dist/bundle.handler --runtime nodejs16.x \
+	  --zip-file fileb://bundle.zip --handler bundle.handler --runtime nodejs16.x \
 	  --role arn:aws:iam::843942154494:role/$lambda_name
 	} || aws lambda update-function-code --function-name $lambda_name --zip-file fileb://bundle.zip
 	rm bundle.zip
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const config = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		libraryTarget: 'commonjs2',
 	},
 	module: {
 		rules: [
