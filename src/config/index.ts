66ff396ceb8d83f016276a7c1bc1946f59a46247
Lucas <lucas.98sf@gmail.com>
2022-07-14 13:45:25 -0300

    fix: config envs

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-	AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-	SQS_QUEUE_URL: process.env.SQS_QUEUE_URL,
+	AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+	AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+	SQS_QUEUE_URL: process.env.SQS_QUEUE_URL!,
 };
 
 export default config;
